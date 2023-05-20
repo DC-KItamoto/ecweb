11f71c6e1ce4612a65757540ccf7b92071a03d38
amine-d <amine.doublali@g.dreamcareer.co.jp>
2023-05-20 23:12:01 +0900

    GetIdの追加

--- src/main/java/com/example/demo/EcController.java
+++ src/main/java/com/example/demo/EcController.java
@@ -3,17 +3,18 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class EcController {
 	@RequestMapping("/index")
 	public String top(Model model) {
-		model.addAttribute("message","Hello world");
+		model.addAttribute("message", "Hello world");
 		return "index";
 	}
-	
+
 	@RequestMapping("/login")
-	public String login(Users users,Model model) {
-		model.addAttribute("login","Login");
+	public String login(Users users, Model model) {
+		model.addAttribute("login", "Login");
 		return "login/input";
 	}
 }
