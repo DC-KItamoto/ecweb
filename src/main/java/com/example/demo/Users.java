e13ea2f4e3eef4f48b90dbb4e321fa00ce5648a1
k_itamoto <konosuke.itamoto@g.dreamcareer.co.jp>
2023-05-20 20:55:44 +0900

    HTMLの値を取得したり、HTMLへ送るために使う値の一時保管場所

---
+++ src/main/java/com/example/demo/Users.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+//ユーザー情報
+public class Users {
+//viewで使える変数
+	private String email;
+	private String name;
+	private String password;
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
