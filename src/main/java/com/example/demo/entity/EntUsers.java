f55c79b29a815bad129b1861210cba1fd6d7b338
k_itamoto <konosuke.itamoto@g.dreamcareer.co.jp>
2023-05-20 20:57:21 +0900

    sqlに接続するときに使う値を一時保管する場所

---
+++ src/main/java/com/example/demo/entity/EntUsers.java
@@ -0,0 +1,27 @@
+package com.example.demo.entity;
+
+public class EntUsers {
+	//daoに行き来する変数
+	private String email;
+	private String name;
+	private String password;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
